9d1b6cb1df53a8697409a4da1898cdad13710be6
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-07-24 15:08:55 -0700

    Update resume files

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,7 +1,30 @@
 export default function Resume() {
     return (
         <>
-        <h4>Resume</h4>
+        <div className="resume">
+        <h2>Resume</h2>
+        <h3>Front-end Proficiencies</h3>
+        <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>JQuery</li>
+            <li>Responsive Design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+            <li>Handlebars</li>
+            </ul>
+            <h3>Back-end Proficiencies</h3>
+            <ul>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL, Sequelize</li>
+            <li>MongoDB, Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+            </ul>
+        </div>
         </>
     )
 }
