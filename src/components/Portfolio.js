453877360ad34a7ad7a88ada7349e5363a80740e
Brian Autieri <119701025+Brian-Autieri@users.noreply.github.com>
2023-08-07 20:43:50 -0700

    Upload final project

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,12 +4,18 @@ import pwaTextEditor from '../assets/images/pwaTextEditor.png';
 import passwordGenerator from '../assets/images/passwordGenerator.png';
 import codeQuiz from '../assets/images/codeQuiz.png';
 import nosql from '../assets/images/nosql.png';
+import HOMEZ from '../assets/images/HOMEZ.png';
 
 export default function Portfolio() {
     return (
         <>
         <div className='portfolioStyle'>
         <h2>Portfolio</h2>
+        <h3>HOMEZ</h3>
+        <img src={HOMEZ} alt="HOMEZ" className='applicationImages' />
+        <p><a href="https://homez-a7bb0a2adb47.herokuapp.com/" target="_blank">Deployed Application</a></p>
+        <p><a href="https://github.com/dustinii/HOMEZ" target="_blank">GitHub Repository</a></p>
+        <br />
         <h3><strong>B</strong>urgers <strong>D</strong>elivered <strong>B</strong>y <strong>S</strong>pace <strong>M</strong>onkeys (<strong>BDSM</strong>)</h3>
         <img src={bdsmImage} alt="BDSM" className='applicationImages' />
         <p><a href="https://polar-cove-07937.herokuapp.com/" target="_blank">Deployed Application</a></p>
